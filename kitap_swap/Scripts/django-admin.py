6f30e564293fa42821ae1e3986d7850cd183179d
neotokyo2019 <tleusher@gmail.com>
2019-05-02 23:00:08 +0600

    Added .gitignore

---
+++ kitap_swap/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\tleusher\documents\github\web-project\webdev\kitap_swap\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
