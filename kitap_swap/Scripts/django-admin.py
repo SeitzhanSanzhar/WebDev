511dbc985b79b3869a55bfa3abb2f957724636f9
neotokyo2019 <tleusher@gmail.com>
2019-05-06 18:38:58 +0600

    Amended the project structure

--- kitap_swap/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\tleusher\documents\github\web-project\webdev\kitap_swap\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
