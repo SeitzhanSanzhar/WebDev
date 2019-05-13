f4ca6f2b64bc6ac0192ef0816dcb71895ba092dd
Sanzhar <SeitzhanSanzhar@gmail.com>
2019-05-13 15:00:30 +0600

    profile add

--- back/api/models.py
+++ back/api/models.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import User
 from django.db import models
-
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 class Book(models.Model):
     name = models.CharField(max_length=200)
@@ -10,6 +11,9 @@ class Book(models.Model):
     genre = models.CharField(max_length=50)
     created_by = models.ForeignKey(User, on_delete=models.CASCADE, default=None, null=True)
 
+    def __str__(self):
+        return self.name;
+
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
@@ -17,3 +21,26 @@ class Post(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     book = models.ForeignKey(Book, on_delete=models.CASCADE, default=None, null=True)
     created_by = models.ForeignKey(User, on_delete=models.CASCADE, default=None, null=True)
+    image = models.ImageField(upload_to='post_images')
+
+    def __str__(self):
+        return self.title;
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(max_length=500, blank=True)
+    location = models.CharField(max_length=30, blank=True)
+    birth_date = models.DateField(null=True, blank=True)
+
+    def __str__(self):
+        return str(self.user.id);
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
+
